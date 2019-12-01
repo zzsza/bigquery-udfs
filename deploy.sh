67757b9f7dff6e096177320e769d41111440d3ef
zzsza <zzsza@naver.com>
2019-12-02 01:29:00 +0900

    [modify] 대문자

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 
 PROJECT_ID="project_id"
 DATASET="dataset"
-file_path='your_path.js'
+FILE_PATH='your_path.js'
 
 
-sed -i.sql -e "s/your_project_id/${PROJECT_ID}/g" -e "s/your_dataset/${DATASET}/g" -e "s/file_path/'${file_path}/g" h3/*
+sed -i.sql -e "s/your_project_id/${PROJECT_ID}/g" -e "s/your_dataset/${DATASET}/g" -e "s/file_path/'${FILE_PATH}/g" h3/*
 
 # Iterate over all SQLs and run them in BQ
 find "$(pwd)" -name "*.sql" | sort  -z |while read fname; do
