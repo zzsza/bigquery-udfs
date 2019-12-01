160d313cdb227bc443f7bff50d5c9517db9cfb82
zzsza <zzsza@naver.com>
2019-12-02 01:46:06 +0900

    [update] files

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PROJECT_ID="project_id"
-DATASET="dataset"
-FILE_PATH='your_path.js'
+PROJECT_ID="custom_project_id"
+DATASET="custom_dataset"
+FILE_PATH='custom_file_path'
 
 
-sed -i.sql -e "s/your_project_id/${PROJECT_ID}/g" -e "s/your_dataset/${DATASET}/g" -e "s/file_path/'${FILE_PATH}/g" h3/*
+sed -i.temp -e "s/your_project_id/${PROJECT_ID}/g" -e "s/your_dataset/${DATASET}/g" -e "s/file_path/${FILE_PATH}/g" h3/*
 
 # Iterate over all SQLs and run them in BQ
 find "$(pwd)" -name "*.sql" | sort  -z |while read fname; do
-  echo "$fname"
-  bq --project_id ${PROJECT_ID} query --use_legacy_sql=false --flagfile=$fname
+ echo "$fname"
+ bq --project_id ${PROJECT_ID} query --use_legacy_sql=false --flagfile=$fname
 done
