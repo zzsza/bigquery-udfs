b2374ef8f64661e720c200ec72f6af7537d28f69
Sung Yun Byeon <zzsza@naver.com>
2019-12-02 11:36:26 +0900

    Update h3_to_children.sql

--- h3/h3_to_children.sql
+++ h3/h3_to_children.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION your_project_id.your_dataset.h3_to_cyour_project_idld
  RETURNS ARRAY<STRING>
  LANGUAGE js AS
 """
-return h3.h3ToCyour_project_idldren(h3_index,resolution);
+return h3.h3ToChildren(h3_index,resolution);
 """
 OPTIONS (
   library=['gs://file_path']
