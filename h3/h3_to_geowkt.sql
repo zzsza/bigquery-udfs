76a55ebefe818cf909fa71b9105238524b58a604
Sung Yun Byeon <zzsza@naver.com>
2019-12-02 11:41:50 +0900

    Update h3_to_geowkt.sql

--- h3/h3_to_geowkt.sql
+++ h3/h3_to_geowkt.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION your_project_id.your_dataset.h3_to_geowkt(h3_index ST
   RETURNS STRING
   LANGUAGE js AS
 """
-var p = h3.h3ToGeo(h3_index);
+var p = h3.h3ToGeo(h3_index);\n
 return "POINT("+p[1]+" "+p[0]+")";
 """
 OPTIONS (
