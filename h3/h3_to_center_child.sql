3c7333e3a3aa0396ff15510d1f5aa8a70da8887e
Sung Yun Byeon <zzsza@naver.com>
2019-12-02 11:58:33 +0900

    Update h3_to_center_child.sql

--- h3/h3_to_center_child.sql
+++ h3/h3_to_center_child.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE FUNCTION your_project_id.your_dataset.h3_to_center_cyour_project_idld(h3_index STRING, resolution NUMERIC)
+CREATE OR REPLACE FUNCTION your_project_id.your_dataset.h3_to_center_child(h3_index STRING, resolution NUMERIC)
  RETURNS STRING
  LANGUAGE js AS
 """
-return h3.h3ToCenterCyour_project_idld(h3_index,resolution);
+return h3.h3ToCenterChild(h3_index,resolution);
 """
 OPTIONS (
   library=['gs://file_path']
