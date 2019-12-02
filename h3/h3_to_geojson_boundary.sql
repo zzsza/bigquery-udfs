63df600ec54bb1414efbc9e9e51f717de85fb1c5
Sung Yun Byeon <zzsza@naver.com>
2019-12-02 11:41:26 +0900

    Update h3_to_geojson_boundary.sql

--- h3/h3_to_geojson_boundary.sql
+++ h3/h3_to_geojson_boundary.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE FUNCTION your_project_id.your_dataset.h3_to_geojson_boundary(h
 var geojson = {
     "type": "Polygon", 
     "coordinates": [h3.h3ToGeoBoundary(h3_index,true)]
-}
+}\n
 
 return JSON.stringify(geojson);
 """
